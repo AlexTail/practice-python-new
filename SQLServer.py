0ac90e5fe25f1ff816f8e829c55bb5efaccf5f68
Alexander Kh <alexandertail89@gmail.com>
2019-06-17 14:03:41 +0700

    Created and added file SQLServer.py

---
+++ SQLServer.py
@@ -0,0 +1,40 @@
+'''
+
+Подключение к SQL Server (локально) и выполнение select
+
+'''
+
+
+import pypyodbc
+
+
+driver = 'Driver={SQL Server}'
+server = 'Server=computer_name\SQLEXPRESS' #localhost  
+database = 'Database=Northwind'
+conn_str = ';'.join([driver, server, database])
+# port
+# user
+# psw
+try:
+    connection = pypyodbc.connect(conn_str)
+    print('connection - done', '\n')
+    cursor = connection.cursor()
+    mySQLQuery = """
+        SELECT CompanyName, ContactName, Country
+        FROM  dbo.Customers
+        WHERE country = 'UK'
+    """
+    cursor.execute(mySQLQuery)
+    results = cursor.fetchall()
+    
+    for row in results:
+        comp_name = row[0]
+        cont_name = row[1]
+        country = row[2]
+        print(' | '.join([comp_name, cont_name, country]), '\n')
+except Exception as ex:
+    print(ex)
+finally:
+    connection.close()
+    print('\n', 'connection - close')
+  
