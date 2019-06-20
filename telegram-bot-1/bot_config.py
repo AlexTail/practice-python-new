6690e16e3f97650a4f4245aef54e88213d9721fd
Alexander Kh <alexandertail89@gmail.com>
2019-06-20 18:27:38 +0700

    Modified bot_config.py

--- telegram-bot-1/bot_config.py
+++ telegram-bot-1/bot_config.py
@@ -13,22 +13,3 @@ Enter your TOKEN that you received from BotFather in Telegram
 SOCKS_PROXY = 'socks5h://52.178.149.16:3333'
 HTTPS_PROXY = 'https://193.193.240.37:51380'
 TOKEN = ''
-
-
-
-
-
-
-
-
-
-
-
-
-#HTTP_PROXY ='http://uEP3gh:ZDFX3o@45.77.91.16:21327'
-#SOCKS_PROXY ='socks5h://45.77.91.16:21328' #http://spys.one/socks/ тут взял рабочие
-
-#45.77.91.16:21328
-#173.244.200.156:61160
-#139.59.169.246:1080
-#TOKEN = '704625224:AAGWLPllIy08-GP3o7WT5hbLdbkqO2O6geM'
