83f201182a54396189a0e4a6bd933675d5cd8c46
Alexander Kh <alexandertail89@gmail.com>
2019-10-17 11:52:56 +0300

    Corrected the name of the folder

---
+++ telegram_bot_1/bot_config.py
@@ -0,0 +1,15 @@
+"""
+
+Здесь взял proxy: http://spys.one/socks/
+Введите свой TOKEN, который получили у BotFather в Telegram
+
+Took a proxy here: http://spys.one/socks/
+Enter your TOKEN that you received from BotFather in Telegram
+
+"""
+
+
+
+SOCKS_PROXY = 'socks5h://52.178.149.16:3333'
+HTTPS_PROXY = 'https://193.193.240.37:51380'
+TOKEN = ''
